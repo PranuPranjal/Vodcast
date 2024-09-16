29672c48456f497589664903f1236c36a55f4d8a
Techno-Meta <98056060+Techno-Meta@users.noreply.github.com>
2024-09-16 13:11:39 +0530

    noice

--- public/info.js
+++
@@ -1,9 +0,0 @@
-const { response } = require("express")
-
-document.getElementById('navbar').addEventListener('click', ()=>{
-    fetch('/user')
-    .then(response => response.text)
-    .then(data =>{
-        document.getElementById('message').innerText = data;
-    });
-});
